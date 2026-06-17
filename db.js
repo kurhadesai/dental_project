51691c8f59c92c927b9bd96fd1248b77c31ece3a
sai kurhade <saikurhade9512@gmail.com>
2026-06-17 17:54:34 +0530

    Merge changes

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Root',
-    database:'dental'
+    host:'b9wisajnwpwzuwo5es8i-mysql.services.clever-cloud.com',
+    user:'u0tvublduqwzhrup',
+    password:'Ii3DsFesklf7pWjyvlAH',
+    database:'b9wisajnwpwzuwo5es8i'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
